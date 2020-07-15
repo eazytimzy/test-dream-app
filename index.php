17476835775de4cc294b33872ac58e7cfc9d54ed
eazytimzy <61963475+eazytimzy@users.noreply.github.com>
2020-07-15 18:43:28 +0100

    correct update

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-  <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
   <title>Dreamland-Landing page</title>
@@ -13,98 +13,73 @@
   <link rel="stylesheet" href="./assets/css/style.css"> -->
 </head>
 <body>
-	<?php 
-	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	//server connection details
-		$servername = "us-cdbr-east-02.cleardb.com";//insert the name of your server
-		$username = "bd18dcb727a755"; //insert your mysql username
-		$password = "96f30e8c"; //insert your password
-		$dbname = "heroku_12f74203d5ae84d"; //insert your database name
-
-		//establish new connection to mysql database
-		$conn = new mysqli($servername, $username, $password, $dbname);
-
-		$email = $error = $responseMsg = "";
-
-		//test input to ensure it is secure and free from any form of injections
-		function test($data) {
-			$data = trim($data);
-			$data = stripslashes($data);
-			$data = htmlspecialchars($data);
-			return $data;
-		}
-
-		/*uncomment this block of code if you want to create the table where the emails will be stored, don't bother if you
-		already have the table*/
-
-  	 	$table = "CREATE TABLE mails (
-  	 		id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  	 		mails VARCHAR(100) NOT NULL
-  	 )";
-
-  	 if($conn->query($table) == true) {
-  	 	echo "Table created";
-  	 } else {
-  	 	echo "Error creating " .$conn->error;
- 	}
-
-		//when the submit button is clicked
-		if(isset($_POST["submit"])) {
-		//if mail field is empty	
-			if(empty( $_POST["email"])) {
-				$error = "The mail field is required";
-			} else {
-				$email = test( $_POST["email"]);
-
-				//checks if email is a valid address
-				if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-					$error = "Invalid email format";
-				} else {
-					//inserts validated email value to database and returns response message
-					$sql = "INSERT INTO mails (mail) VALUES ('$email')";
-					$conn->query($sql);
-					
-					`$insert_email = $conn->query($sql);
-						if ($insert_email) {
-						"submited to databas sucessfuly"
-				}
-					else{
-					"ERROR submiting to database"
-				}`
-					//return response message
-					$responseMsg ="Thank you, we promise to reach out";
-				}
-			}
-		}
-	}
-	?>
-
-
-
-
+    <?php 
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //server connection details
+        $servername = "us-cdbr-east-02.cleardb.com";//insert the name of your server
+        $username = "bd18dcb727a755"; //insert your mysql username
+        $password = "96f30e8c"; //insert your password
+        $dbname = "heroku_12f74203d5ae84d"; //insert your database name
+        //establish new connection to mysql database
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        $email = $error = $responseMsg = "";
+        //test input to ensure it is secure and free from any form of injections
+        function test($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+        /*uncomment this block of code if you want to create the table where the emails will be stored, don't bother if you
+        already have the table*/
+        /*$table = "CREATE TABLE mails (
+            id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            mails VARCHAR(100) NOT NULL
+     )";
+     if($conn->query($table) == true) {
+        echo "Table created";
+     } else {
+        echo "Error creating " .$conn->error;
+    }*/
+        //when the submit button is clicked
+        if(isset($_POST["submit"])) {
+        //if mail field is empty    
+            if(empty( $_POST["email"])) {
+                $error = "The mail field is required";
+            } else {
+                $email = test( $_POST["email"]);
+                //checks if email is a valid address
+                if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                    $error = "Invalid email format";
+                } else {
+                    //inserts validated email value to database and returns response message
+                    $sql = "INSERT INTO mails (mails) VALUES ('$email')";
+                    $insert_email = $conn->query($sql);
+                        if ($insert_email) {
+                                            //return response message
+                    $responseMsg ="Thank you, we promise to reach out";
+                    }
+                    else{
+                        $error = "Email not saved";
+                }
+                }
+            }
+        }
+    }
+    ?>
           <div class="container-fluid p-0" id="mainbody">
             <a href="#" class="navbar-brand pl-5"><h1 class=""><img src="./assets/images/frame 3.svg" alt="logo.svg"></h1></a>
                 <div class="form-sect">
                   <h2>Enter email address: </h2>
-	                	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST">
-		                  	<input type="text" name="email" placeholder="Enter email" value="<?php echo $email ?>"><br>
-		                      	<span class="error"><?php echo $error ?></span><!-- show errors if any --><br>
-		                        	<button type="submit" name="submit">YES! KEEP ME UPDATED</button>
-		                          	<br>
-			                              <span><?php echo $responseMsg ?></span><!-- returns response message -->
-                 </form>		
+                        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST">
+                            <input type="email" name="email" placeholder="Enter email" value="<?php if (isset($email)) {echo $email;} ?>"><br>
+                                <span class="error"><?php if (isset($error)){ echo $error;} ?></span><!-- show errors if any --><br>
+                                    <button type="submit" name="submit">YES! KEEP ME UPDATED</button>
+                                    <br>
+                                          <span><?php if (isset($responseMsg)) {echo $responseMsg;} ?></span><!-- returns response message -->
+                 </form>        
             </div>
-
      </div>
-
-
-
-
-
-
-
-
-    
     <script src="./assets/javascript/main.js"></script>
 </body>
 </html>
