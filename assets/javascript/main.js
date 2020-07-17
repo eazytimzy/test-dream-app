b213a4ad52279dc3768a9b8984b22e0853e378f7
eazytimzy <61963475+eazytimzy@users.noreply.github.com>
2020-07-17 01:45:04 +0100

    assets update

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,98 @@
 
+@media screen and (max-width: 578px){
+  #mainbody{
+    overflow-x: hidden;
+  }
+  .margin-add{
+    margin-left:20px !important;
+}
+
+.moon-img{
+    left: 50.24%;
+    right: 14.07%;
+    top: 0.72%;
+    bottom: 92.4%;
+}
+.star-img{
+    left: 78.34%;
+    right: 17.98%;
+    top: 2.25%;
+    bottom: 94.58%;
+}
+
+.cloud-one{
+  display: none; 
+}
+.cloud-two{
+    display: none;
+}
+.cloud-three{
+    display: none;
+}
+.fish-img{
+    position: absolute;
+    /* left: 50%; */
+    top: 60%;
+    /* bottom: 500px; */
+    width: 300px;
+}
+.fish-div{
+     /* right: 2%; */
+    top: 15%;
+    bottom:;
+    /* right: 800px; */
+    left: 120px;
+    -ms-transform: rotate(320deg);
+  transform: rotate(320deg);
+   
+}
+.text-head{
+  width:300px !important;
+}
+}
+.break, .break-sec{
+  display: none;
+}
+.text-pack{
+  width: 80% !important;
+}
+.update{
+  width: 300px !important;
+  /* margin-top: 35px !important; */
+  margin-bottom: -30px !important;
+}
+.form-container .inpt[type = email]{
+  width: 300px;
+  border: 1px solid #D9D1B8;
+  background-color: #021859;
+  outline: none;
+  border-top-color: #021859;
+  border-left-color: #021859;
+  border-right-color: #021859;
+  color: #D9D1B8;
+}
+.row-mob{
+  margin-left: 0px !important;
+}
+.mob-pad-b{
+  padding-bottom: 30px;
+}
+.head-2{
+  width: 300px;
+  margin-left:0px;
+}
+.form-words{
+  width: 300px;
+  /* margin-left:0px; */
+}
+form .input{
+  width: 300px;
+}
+form .input-btn{
+  padding: 8px 5px 8px 5px;
+  margin-top:-30px !important;
+  width: 300px;
+}
+.heading{
+  margin-top: -100px !important;
+}
